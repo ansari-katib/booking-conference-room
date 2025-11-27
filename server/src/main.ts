52ce418ce6fc5b7a2aecb0b9b57657189489a0ef
KatibAnsariTech <katib.ansari@spacetotech.com>
2025-11-27 20:44:30 +0530

    .

--- server/src/main.ts
+++ server/src/main.ts
@@ -6,11 +6,12 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors({
-    origin: "https://booking-conference-room-alpha.vercel.app",
-    // origin: [process.env.FRONTEND_BASE_URL, 'http://localhost:3000'],
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    origin: ['https://booking-conference-room-alpha.vercel.app'],
     credentials: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Authorization',
   });
+
   await app.listen(process.env.PORT ?? 5000);
 }
 bootstrap();
