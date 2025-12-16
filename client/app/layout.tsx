afe8e1531bca1c6a12f00ccdb8b75cdab5e5b762
KatibAnsariTech <katib.ansari@spacetotech.com>
2025-12-16 16:08:11 +0530

    implemented PWA feature

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,19 +1,32 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { BookingsProvider } from "@/context/BookingsContext";
 import "./globals.css";
+import RegisterSW from "./register-sw";
 
 export const metadata: Metadata = {
   title: "Book Conference Room",
   description: "Book conference room",
+  generator: "Next.js",
+  manifest: "/manifest.json",
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
+  themeColor: "#ffffff",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
-      <body>
+      <body className="antialiased">
+        <RegisterSW />
         <BookingsProvider>{children}</BookingsProvider>
       </body>
     </html>
   );
 }
-
---
+++ client/public/sw.js
@@ -0,0 +1,11 @@
+self.addEventListener("install", (event) => {
+  console.log("SW installed");
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  console.log("SW activated");
+  clients.claim();
+});
+
+self.addEventListener("fetch", () => {});
